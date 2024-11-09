28925dabcb3d0f8e70e9a264917a600cfb11f545
JaeHyun <vv501610@gmail.comm>
2024-11-10 02:11:06 +0900

    10주차 과제

---
+++ 10주차/char_data.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define SIZE 100
+int main() {
+    char input[SIZE];
+    FILE* fp = NULL;
+    int i = 0;
+    if((fp=fopen("output.bin", "rb")) == NULL) {
+        printf("error...");
+        return 0;
+    }
+
+    while(!feof(fp)){
+        fread(&input[i], sizeof(char), 1, fp);
+        i++;
+    }
+    input[--i] = '\0';
+    puts(input);
+    fclose(fp);
+    return 0;
+}
