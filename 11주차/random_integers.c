e8a156343008e04b03f76171b9fce1128cdfabf2
JaeHyun <vv501610@gmail.comm>
2024-11-21 04:40:22 +0900

    11주차 과제

---
+++ 11주차/random_integers.c
@@ -0,0 +1,21 @@
+
+int main(void){
+	char* pChar;
+	int* pInt;
+	int count;
+
+	scanf("%d", &count);
+	pInt = (int*)malloc(count*sizeof(int));
+	
+	for (int i = 0; i < count; i++) pInt[i] = rand() % 100;
+	for (int i = 0; i < count; i++) printf("%d ", pInt[i]);
+	
+	pChar = (char*)pInt;
+
+	while (getchar() != '\n');
+	gets(pChar);
+	puts(pChar);
+
+	free(pInt);
+	return 0;
+}
